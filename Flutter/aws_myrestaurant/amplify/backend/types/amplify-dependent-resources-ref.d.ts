4683acee054295412de264afd2e2c8537007a479
rocosrex <rocos.rex@gmail.com>
2023-01-03 00:35:03 +0900

    apply aws
    auth, storage in ap-northeast-2

---
+++ Flutter/aws_myrestaurant/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsmyrestaurant4c5fa1b1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger6a97748d": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "zLINsRRPcxuYTACbe": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
